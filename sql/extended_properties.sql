931c4632914c0b69e495edf2682bbac4c26e5259
Paul Harrington <phrrngtn@panix.com>
2022-10-27 20:31:38 -0400

    remove defunct Split function

--- sql/extended_properties.sql
+++ sql/extended_properties.sql
@@ -89,25 +89,6 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-CREATE FUNCTION [RULE4].[Split] (@sep char(1), @s varchar(512))
-RETURNS table
-AS
--- XXX: will break if there are more than 100 occurances of @sep (maximum level
--- of recursion)
-RETURN (
-    WITH Pieces(pn, start, stop) AS (
-      SELECT 1, 1, CHARINDEX(@sep, @s)
-      UNION ALL
-      SELECT pn + 1, stop + 1, CHARINDEX(@sep, @s, stop + 1)
-      FROM Pieces
-      WHERE stop > 0
-    )
-    SELECT pn,
-      SUBSTRING(@s, start, CASE WHEN stop > 0 THEN stop-start ELSE 512 END) AS s
-    FROM Pieces
-  )
-GO
-
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
